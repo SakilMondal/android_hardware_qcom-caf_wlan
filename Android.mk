34f28e2b09d5c76a2ab6d00577159a280f83d135
Ethan Halsall <ethan.halsall@gmail.com>
2020-09-16 23:05:20 +0100

    Make sure caf wlan hal is only compilied when using QCOM_HARDWARE
    Stops conflict with aosp wlan hal
    
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,6 @@
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+ifeq ($(BOARD_USES_QCOM_HARDWARE), true)
 include $(call all-subdir-makefiles)
+endif
 endif
